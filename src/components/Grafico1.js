5b68f1aa4dd4f8ddb8a2ed59a67eef7dcb19473f
TuscioWang <tuscio97@live.it>
2021-04-09 17:36:32 +0200

    assi cambiati

--- src/components/Grafico1.js
+++ src/components/Grafico1.js
@@ -3,6 +3,7 @@ import { Group } from '@visx/group';
 import { Bar } from '@visx/shape';
 import { scaleLinear, scaleBand } from '@visx/scale';
 import { AxisBottom, AxisLeft } from '@visx/axis';
+import {Grid } from '@visx/grid';
 //import { timeParse } from 'd3-time-format';
 
 
@@ -25,18 +26,16 @@ const margin = { top: 20, bottom: 20, left: 20, right: 20 };
 // Then we'll create some bounds
 const xMax = width - margin.left - margin.right;
 const yMax = height - margin.top - margin.bottom;
-console.log("y", yMax,"X",xMax);
 
 // We'll make some helpers to get at the data we want
 const x = d => d.date;
-const y = d => d.datoScelto  ;
+const y = d => d.datoScelto;
 
 // And then scale the graph by our data
 const xScale = scaleBand({
   range: [0, xMax],
   round: true,
-  domain: data.map(x),
-  padding: 0.2,
+  domain: data.map(x), 
 });
 
 const yScale = scaleLinear({
@@ -53,47 +52,49 @@ const yPoint = compose(yScale, y);
 // Finally we'll embed it all in an SVG
   return (
     <svg width={width} height={height}>
-      {data.map((d, i) => {
-        const barHeight = yMax - yPoint(d);
-        return (
-          <Group key={`bar-${i}`}>
-            <Bar
-            top={50}
-              x={xPoint(d) + 50}
-              y={yMax - barHeight}
-              height={barHeight}
-              width={xScale.bandwidth()}
-              fill="#fc2e1c"
-
-            />
-
-            <AxisLeft 
-            left={50}
-            label={selected}
-            scale={yScale} 
-            tickLabelProps={() => ({
-              fontSize: 11,
-              textAnchor: 'end',
-              dy: '0.50em',
-
-            })}
-            />
-            
-            <AxisBottom
-            left={50}
-              top={yMax}
-              scale={xScale}
-              numTicks={14}
-              label={"Data"}
-              tickLabelProps={() => ({
-                fontSize: 11,
-                textAnchor: 'middle',
-                dy: '0.20em',
-              })}
-            />
-          </Group>
-        );
-      })}
+      <Group key={"group-graph"} left={margin.left}>
+        {data.map((d, i) => {
+          const barHeight = yMax - yPoint(d) ;
+          return (
+            <Group key={`bar-${i}`}>
+              <Bar
+                x={xPoint(d)}
+                y={yMax - barHeight}
+                height={barHeight}
+                width={xScale.bandwidth()}
+                fill="#fc2e1c"
+              />
+            </Group>
+          );
+        })}
+      <GridRows />
+      <AxisLeft 
+        left={40}
+        label={selected}
+        labelProps={()=>({
+          fontSize: 11,
+          dy: '0.40em',
+        })}
+        scale={yScale} 
+        tickLabelProps={() => ({
+          fontSize: 11,
+          textAnchor: 'end',
+          dy: '0.40em',
+        })}
+      />
+          
+      <AxisBottom
+        top={yMax}
+        scale={xScale}
+        numTicks={14}
+        label={"Data"}
+        tickLabelProps={() => ({
+          fontSize: 11,
+          textAnchor: 'middle',
+          dy: '0.40em',
+        })}
+      />
+      </Group>
     </svg>
   );
 }
